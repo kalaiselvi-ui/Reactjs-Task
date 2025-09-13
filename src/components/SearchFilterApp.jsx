2216be1e992f52cf40df5ec2056119621d6787da
Kalai <kalaichandran369@gmail.com>
2025-09-13 11:02:43 +0400

    filter movie added

--- src/components/SearchFilterApp.jsx
+++ src/components/SearchFilterApp.jsx
@@ -12,6 +12,7 @@ const SearchFilterApp = () => {
   const [input, setInput] = useState("");
   const [movieName, setMovieName] = useState("");
   const [movieList, setMovieList] = useState([]);
+  const [filterList, setFilterList] = useState([]);
   const NameList = [
     {
       id: "1",
@@ -69,18 +70,37 @@ const SearchFilterApp = () => {
     }
   }, [input]);
 
-  const searchMovieHandle = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.get(
-        `https://api.themoviedb.org/3/discover/movie?api_key=7076dcc65497f0c127b05d81daf4b8c9&language=en-US&with_original_language=ta&sort_by=popularity.desc&page=1`
-      );
-      if (response.data) {
-        console.log(response);
+  useEffect(() => {
+    const TamilMovieList = async () => {
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=7076dcc65497f0c127b05d81daf4b8c9&language=en-US&with_original_language=ta&sort_by=popularity.desc&page=1`
+        );
+        if (response.data) {
+          console.log(response.data);
+          setMovieList(response.data.results);
+          setFilterList(response.data.results);
+        }
+      } catch (err) {
+        console.log(err);
       }
-    } catch (err) {
-      console.log(err);
-    }
+    };
+    TamilMovieList();
+  }, []);
+
+  const searchMovieHandle = (e) => {
+    console.log(movieList, "working");
+    const value = e.target.value;
+    setMovieName(value);
+
+    const filtered = value
+      ? movieList.filter((item) =>
+          item.title.toLowerCase().includes(value.toLowerCase())
+        )
+      : movieList;
+    console.log(filtered, "filterlist");
+
+    setFilterList(filtered);
   };
 
   return (
@@ -116,41 +136,51 @@ const SearchFilterApp = () => {
         <h3 className="font-semibold text-2xl mt-10 mb-5">
           2. Search Filter by Movie using API
         </h3>
-        <form
-          className="flex justify-center items-center"
-          onSubmit={searchMovieHandle}
-        >
+        <form className="flex justify-center items-center">
           <input
             type="text"
             placeholder="Search movie"
             className="max-w-xl w-full pl-5 border border-green-500 h-10"
             value={movieName}
             name="movieName"
-            onChange={(e) => setInput(e.target.value)}
+            onChange={searchMovieHandle}
           />
-          <button
-            type="submit"
-            className="px-10 py-2 bg-black text-white rounded-r-md"
-          >
+          {/* <button className="px-10 py-2 bg-black text-white rounded-r-md">
             {" "}
             Submit
-          </button>
+          </button> */}
         </form>
 
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 lg:gap-6 gap-3 mt-10">
-          {movieList.map((item) => (
+          {filterList.map((item) => (
             <card key={item.id}>
               <div className="rounded-t-md">
                 <img
                   className="object-cover w-full h-80 rounded-t-md"
-                  src={item.image}
-                  alt
+                  src={
+                    item?.poster_path
+                      ? `https://image.tmdb.org/t/p/w500/${item.poster_path}`
+                      : "https://static.toiimg.com/thumb/imgsize-23456,msid-71832899,width-600,resizemode-4/71832899.jpg"
+                  }
+                  alt={item?.title || "Movie Poster"}
                 />
               </div>
-              <div className="bg-gray-200 flex flex-col gap-1 py-5 rounded-b-md">
-                <p className="text-base font-medium">{item.name}</p>
-                <p className="text-gray-700 text-sm">{item.desc}</p>
-                <p className="text-md font-normal">{item.year}</p>
+              <div className="bg-gray-200 flex flex-col gap-1 py-5 rounded-b-md px-3">
+                <p className="text-base font-medium">
+                  <span className="text-black-500 line-clamp-1">
+                    {item.title}
+                  </span>
+                </p>
+                <p className="text-gray-700 text-sm line-clamp-2">
+                  <span className="text-gray-500 text-xs space-x-0">
+                    {" "}
+                    {item.overview}
+                  </span>
+                </p>
+                <p className="text-md font-normal">
+                  Rating:{" "}
+                  <span className="text-red-500">{item.vote_average}</span>
+                </p>
               </div>
             </card>
           ))}
